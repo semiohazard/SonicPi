67e16f99ab75e0e49e2d5ffca7f0b088a0836bd7
semiohazard <99359071+semiohazard@users.noreply.github.com>
2022-03-29 10:32:18 -0700

    Create CustomSong2.rb

---
+++ CustomSong2.rb
@@ -0,0 +1,54 @@
+use_synth :saw
+
+
+live_loop :riff do
+  play :c4
+  sleep 0.5
+  play :e4
+  sleep 0.5
+  play :c4
+  sleep 0.25
+  play :e4
+  sleep 0.5
+  play :a3
+  sleep 0.5
+  play :a3
+  sleep 0.25
+  play :e4
+  sleep 0.25
+  play :a3
+  sleep 0.25
+  play :a3
+  sleep 0.25
+  play :f4
+  sleep 0.25
+  play :e4
+  sleep 0.25
+  play :e4
+  sleep 0.25
+  
+  #4
+  
+  play :c4
+  sleep 0.5
+  play :e4
+  sleep 0.5
+  play :c4
+  sleep 0.25
+  play :e4
+  sleep 0.5
+  play :a3
+  sleep 0.5
+  play :a3
+  sleep 0.5
+  play :a3
+  sleep 0.25
+  play :c4
+  sleep 0.25
+  play :c4
+  sleep 0.25
+  play :c4
+  sleep 0.25
+  play :c4
+  sleep 0.25
+end
