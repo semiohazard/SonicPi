f42e59e7d8033099e3f6359043f368f04de0e40d
semiohazard <99359071+semiohazard@users.noreply.github.com>
2022-03-29 10:38:01 -0700

    Update CustomSong2.rb

--- CustomSong2.rb
+++ CustomSong2.rb
@@ -1,54 +1,57 @@
-use_synth :saw
+use_synth :rodeo
 
-
-live_loop :riff do
-  play :c4
-  sleep 0.5
-  play :e4
-  sleep 0.5
-  play :c4
-  sleep 0.25
-  play :e4
-  sleep 0.5
-  play :a3
-  sleep 0.5
-  play :a3
-  sleep 0.25
-  play :e4
-  sleep 0.25
-  play :a3
-  sleep 0.25
-  play :a3
-  sleep 0.25
-  play :f4
-  sleep 0.25
-  play :e4
-  sleep 0.25
-  play :e4
-  sleep 0.25
-  
-  #4
-  
-  play :c4
-  sleep 0.5
-  play :e4
-  sleep 0.5
-  play :c4
-  sleep 0.25
-  play :e4
-  sleep 0.5
-  play :a3
-  sleep 0.5
-  play :a3
-  sleep 0.5
-  play :a3
-  sleep 0.25
-  play :c4
-  sleep 0.25
-  play :c4
-  sleep 0.25
-  play :c4
-  sleep 0.25
-  play :c4
-  sleep 0.25
+with_fx :flanger do
+  with_fx :ping_pong do
+    live_loop :riff do
+      play :c4
+      sleep 0.5
+      play :e4
+      sleep 0.5
+      play :c4
+      sleep 0.25
+      play :e4
+      sleep 0.5
+      play :a3
+      sleep 0.5
+      play :a3
+      sleep 0.25
+      play :e4
+      sleep 0.25
+      play :a3
+      sleep 0.25
+      play :a3
+      sleep 0.25
+      play :f4
+      sleep 0.25
+      play :e4
+      sleep 0.25
+      play :e4
+      sleep 0.25
+      
+      #4
+      
+      play :c4
+      sleep 0.5
+      play :e4
+      sleep 0.5
+      play :c4
+      sleep 0.25
+      play :e4
+      sleep 0.5
+      play :a3
+      sleep 0.5
+      play :a3
+      sleep 0.5
+      play :a3
+      sleep 0.25
+      play :c4
+      sleep 0.25
+      play :c4
+      sleep 0.25
+      play :c4
+      sleep 0.25
+      play :c4
+      sleep 0.25
+    end
+  end
 end
