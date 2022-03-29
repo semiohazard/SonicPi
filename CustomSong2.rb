74f844236a81b2ece5251745f665d352058d5845
semiohazard <99359071+semiohazard@users.noreply.github.com>
2022-03-29 11:11:56 -0700

    Update CustomSong2.rb

--- CustomSong2.rb
+++ CustomSong2.rb
@@ -1,60 +1,66 @@
 use_synth :rodeo
 
-with_fx :flanger do
-  with_fx :ping_pong  do
-    live_loop :riff do
-      play :c4
-      sleep 0.5
-      play :e4
-      sleep 0.5
-      play :c4
-      sleep 0.25
-      play :e4
-      sleep 0.5
-      play :a3
-      sleep 0.5
-      play :a3
-      sleep 0.25
-      play :e4
-      sleep 0.25
-      play :a3
-      sleep 0.25
-      play :a3
-      sleep 0.25
-      play :f4
-      sleep 0.25
-      play :e4
-      sleep 0.25
-      play :e4
-      sleep 0.25
-      
-      #4
-      
-      play :c4
-      sleep 0.5
-      play :e4
-      sleep 0.5
-      play :c4
-      sleep 0.25
-      play :e4
-      sleep 0.5
-      play :a3
-      sleep 0.5
-      play :a3
-      sleep 0.5
-      play :a3
-      sleep 0.25
-      play :c4
-      sleep 0.25
-      play :c4
-      sleep 0.25
-      play :c4
-      sleep 0.25
-      play :c4
-      sleep 0.25
+
+live_loop :riff do
+  2.times do
+    with_fx :flanger do
+      with_fx :ping_pong  do
+        play :c4
+        sleep 0.5
+        play :e4
+        sleep 0.5
+        play :c4
+        sleep 0.25
+        play :e4
+        sleep 0.5
+        play :a3
+        sleep 0.5
+        play :a3
+        sleep 0.25
+        play :e4
+        sleep 0.25
+        play :a3
+        sleep 0.25
+        play :a3
+        sleep 0.25
+        play :f4
+        sleep 0.25
+        play :e4
+        sleep 0.25
+        play :e4
+        sleep 0.25
+        
+        #4
+        
+        play :c4
+        sleep 0.5
+        play :e4
+        sleep 0.5
+        play :c4
+        sleep 0.25
+        play :e4
+        sleep 0.5
+        play :a3
+        sleep 0.5
+        play :a3
+        sleep 0.5
+        play :a3
+        sleep 0.25
+        play :c4
+        sleep 0.25
+        play :c4
+        sleep 0.25
+        play :c4
+        sleep 0.25
+        play :c4
+        sleep 0.25
+      end
     end
   end
+  stop
 end
+
+sample :ambi_drone, amp: 4, rate: 0.25
 
 live_loop :drumbeat do
   sample :bd_ada
