1aecfe705c3db6aa35db126c74c8576a6b78009a
semiohazard <99359071+semiohazard@users.noreply.github.com>
2022-04-01 12:08:36 -0700

    Update CustomSong2.rb

--- CustomSong2.rb
+++ CustomSong2.rb
@@ -1,49 +1,52 @@
-use_synth :rodeo
-
 define :riff do |a,b,c,d,e,f|
+  play :c4
+  sleep 0.5
+  play :e4
+  sleep 0.5
+  play :c4
+  sleep 0.25
+  play :e4
+  sleep 0.5
+  play :a3
+  sleep 0.5
+  play :a3
+  
+  sleep a
+  play b
+  sleep c
+  play :a3
+  sleep 0.25
+  play d
+  sleep 0.25
+  play e
+  sleep 0.25
+  play f
+  sleep 0.25
+  play f
+  sleep 0.25
+end
+
+live_loop :riff do
+  use_synth :rodeo
   with_fx :flanger do
     with_fx :ping_pong do
-      play :c4
-      sleep 0.5
-      play :e4
-      sleep 0.5
-      play :c4
-      sleep 0.25
-      play :e4
-      sleep 0.5
-      play :a3
-      sleep 0.5
-      play :a3
-      
-      sleep a
-      play b
-      sleep c
-      play :a3
-      sleep 0.25
-      play d
-      sleep 0.25
-      play e
-      sleep 0.25
-      play f
-      sleep 0.25
-      play f
-      sleep 0.25
+      riff 0.25, :e4, 0.25, :a3, :f4, :e4
+      riff 0, :r, 0.5, :c4, :c4, :c4
     end
   end
 end
 
-live_loop :riff do
-  riff 0.25, :e4, 0.25, :a3, :f4, :e4
-  riff 0, :r, 0.5, :c4, :c4, :c4
-end
 sleep 8
+use_synth :growl
+play :C2, amp: 1, attack: 2, release: 6
+
 live_loop :drone1 do
-  sample :ambi_drone, amp: 2, rate: 0.25
+  sample :ambi_drone, amp: 1, rate: 0.25
   sleep sample_duration :ambi_drone, rate: 0.25
 end
 sleep 4
 live_loop :drone2 do
-  sample :ambi_drone, amp: 2, rate: 0.5
+  sample :ambi_drone, amp: 1, rate: 0.5
   sleep sample_duration :ambi_drone, rate: 0.5
 end
 live_loop :drumbeat do
